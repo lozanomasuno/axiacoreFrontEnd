4596a6d86ae980ead352293059039f2f44352c43
lozanomasuno <39577534+lozanomasuno@users.noreply.github.com>
2022-10-08 17:56:39 -0500

    grid loads data

--- main.js
+++ main.js
@@ -11,17 +11,54 @@ let filterNumber = document.getElementById('filterNotification');
 let filterNotificationmain = document.getElementById('filterNotification-main');
 let allCheckbox = document.querySelectorAll('.checkbox')
 let checksenabled = 0;
-
-
+let productList;
+loadProductsdata = () =>{    
+    fetch('./assets/products.json')
+           .then((res)=>{
+               return res.json();
+           })
+           .then((data) =>{
+               productList = data.products;
+               loadHandlebarsTemplate(  productList ); 
+               createGridPictures();               
+           })
+           .catch( (error) => {
+               console.log(error)
+           })
+           
+   return productList;
+};
 window.onload = () =>{
+
     filterNumber.innerHTML = 0;
     filterNotificationmain.innerHTML = 0;
 
     cbxmorena.checked = false;
     cbxrubia.checked  = false;
-    cbxroja.checked   = false;    
+    cbxroja.checked   = false;  
+    loadProductsdata(); 
     
 };
+
+let createGridPictures = () => {
+    let html = '';
+    let gridBoard = document.getElementById('productGrid');
+
+    productList.forEach((product)=>{
+        html +=`
+        <div class="product-card">
+            <img src="./assets/img/${product.img}" alt =${product.name} />
+            <p> ${product.name}</p>
+            <p> ${product.description}</p>
+            <p>$${product.price}<p>
+            <div class="agregar">
+                AGREGAR
+            </div>
+        </div> 
+         `
+    })
+    gridBoard.innerHTML = html;
+}
 
  howManycheckboxEnabled = () => {
     allCheckbox.forEach( (checkitem)=>{
@@ -40,12 +77,6 @@ window.onload = () =>{
     })
  }
 
- howManycheckboxEnabled();  
-
-
-    
-
-
  clearCheckBox = () => {
     filterNumber.innerHTML = 0;
     filterNotificationmain.innerHTML = 0;
@@ -53,4 +84,12 @@ window.onload = () =>{
     cbxrubia.checked  = false;
     cbxroja.checked   = false;
  }
+ 
+loadHandlebarsTemplate = ( productList )=>{      
+    let template = Handlebars.compile( document.getElementById('product-template').innerHTML);
+    document.getElementById('contentProducts').innerHTML = template(productList);
+    console.log(productList);
+ }
 
+ 
+ howManycheckboxEnabled();  
